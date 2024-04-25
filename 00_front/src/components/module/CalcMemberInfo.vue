aad7423f8857e9bf667376b694327fa55534ab4e
sunadat <sunadat@co.jp>
2024-04-25 11:20:14 +0900

    [fix]MemberInfoのプロパティがDBと一致していなかった不具合の修正

--- 00_front/src/components/module/CalcMemberInfo.vue
+++ 00_front/src/components/module/CalcMemberInfo.vue
@@ -1,72 +1,89 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { parse, format, startOfToday, startOfWeek, startOfMonth, endOfMonth, add, addDays, addMonths } from 'date-fns'
+import {
+  parse,
+  format,
+  startOfToday,
+  startOfWeek,
+  startOfMonth,
+  endOfMonth,
+  add,
+  addDays,
+  addMonths,
+} from "date-fns";
 
 export default {
-  name: 'CalcMemberInfo',
-  methods:{
-    CalcuNewMemberInfo(toubanInfo, selected){
-      var new_memberInfos = []
-      var currentMaxOrder = 0
-      for(var i=0; i<selected.length; i++){
-        var memberInfo = {}
-        memberInfo.touban_id = toubanInfo.id
-        memberInfo.affiliation = selected[i].affiliation
-        memberInfo.name = selected[i].name
-        memberInfo.emplyoee_number = "E" + selected[i].emplyoeeNo   //バックエンド側で数値文字列を上手く扱えないのでE付きに。
-        memberInfo.order_number = currentMaxOrder + 1
-        memberInfo.last = "2099-12-31"  //適当な初期値
-        memberInfo.next = null          //後で計算
-        memberInfo.email = selected[i].email
-        new_memberInfos.push(memberInfo)
+  name: "CalcMemberInfo",
+  methods: {
+    CalcuNewMemberInfo(toubanInfo, selected) {
+      var new_memberInfos = [];
+      var currentMaxOrder = 0;
+      for (var i = 0; i < selected.length; i++) {
+        var memberInfo = {};
+        memberInfo.touban_id = toubanInfo.id;
+        memberInfo.affiliation = selected[i].affiliation;
+        memberInfo.name = selected[i].name;
+        memberInfo.emplyoee_number = "E" + selected[i].emplyoeeNo; //バックエンド側で数値文字列を上手く扱えないのでE付きに。
+        memberInfo.order_number = currentMaxOrder + 1;
+        memberInfo.last = "2099-12-31"; //適当な初期値
+        memberInfo.next = null; //後で計算
+        memberInfo.email = selected[i].email;
+        new_memberInfos.push(memberInfo);
 
-        currentMaxOrder = memberInfo.order_number
+        currentMaxOrder = memberInfo.order_number;
       }
       // メンバー分の次回実施日を計算
-      const data = toubanInfo.scheduling.split("-")
-      const newInterval = parseInt(data[0])
-      const newWeek = parseInt(data[1])
-      const newDay = parseInt(data[2])
-      const newDate = parseInt(data[3])
-      const nextDate = toubanInfo.next
-      new_memberInfos = this.ReSchedule(new_memberInfos, newInterval, newWeek, newDay, newDate, nextDate)
+      const data = toubanInfo.scheduling.split("-");
+      const newInterval = parseInt(data[0]);
+      const newWeek = parseInt(data[1]);
+      const newDay = parseInt(data[2]);
+      const newDate = parseInt(data[3]);
+      const nextDate = toubanInfo.next;
+      new_memberInfos = this.ReSchedule(
+        new_memberInfos,
+        newInterval,
+        newWeek,
+        newDay,
+        newDate,
+        nextDate
+      );
 
-      return new_memberInfos
+      return new_memberInfos;
     },
     // メンバーの入れ替え（※入れ替えのみ行う）
-    ReplaceMember(current_memberInfos, selected){
-      var memberInfos = []
+    ReplaceMember(current_memberInfos, selected) {
+      var memberInfos = [];
 
-      const toubanId = parseInt(current_memberInfos[0].touban_id)   //要素番号はなんでもいい
+      const toubanId = parseInt(current_memberInfos[0].touban_id); //要素番号はなんでもいい
       // 現在のorderの最大値を取得
-      var currentMaxOrder = current_memberInfos.reduce((prev, current) => (prev.order_number > current.order_number) ? prev : current).order_number;
+      var currentMaxOrder = current_memberInfos.reduce((prev, current) =>
+        prev.order_number > current.order_number ? prev : current
+      ).order_number;
 
       // 既存メンバーと新しく選択されたメンバーを照合
-      for(var j=0; j<selected.length; j++){
-        for(var i=0; i<current_memberInfos.length; i++){
+      for (var j = 0; j < selected.length; j++) {
+        for (var i = 0; i < current_memberInfos.length; i++) {
           // 一致する場合は既存メンバーなので既存データをそのままpush
-          if(current_memberInfos[i].name == selected[j].name){
-            memberInfos.push(current_memberInfos[i])
-            break
+          if (current_memberInfos[i].name == selected[j].name) {
+            memberInfos.push(current_memberInfos[i]);
+            break;
           }
           // 最後まで一致する名前が見つからなかった場合は新規メンバー
-          if(i == current_memberInfos.length-1){
-            var memberInfo = {}
-            memberInfo.touban_id = toubanId
-            memberInfo.affiliation = selected[j].affiliation
-            memberInfo.name = selected[j].name
-            memberInfo.emplyoee_number = "E" + selected[j].emplyoeeNo   //バックエンド側で数値文字列を上手く扱えないのでE付きに。
-            memberInfo.order_number = currentMaxOrder + 1
-            memberInfo.last = "2099-12-31"  //適当な初期値
-            memberInfo.next = null          //後で計算
-            memberInfo.mail = selected[j].email
-            memberInfos.push(memberInfo)
-
-            currentMaxOrder = memberInfo.order_number
+          if (i == current_memberInfos.length - 1) {
+            var memberInfo = {};
+            memberInfo.touban_id = toubanId;
+            memberInfo.affiliation = selected[j].affiliation;
+            memberInfo.name = selected[j].name;
+            memberInfo.emplyoee_number = "E" + selected[j].emplyoeeNo; //バックエンド側で数値文字列を上手く扱えないのでE付きに。
+            memberInfo.order_number = currentMaxOrder + 1;
+            memberInfo.last = "2099-12-31"; //適当な初期値
+            memberInfo.next = null; //後で計算
+            memberInfo.email = selected[j].email;
+            memberInfos.push(memberInfo);
+            currentMaxOrder = memberInfo.order_number;
           }
         }
       }
@@ -76,109 +93,147 @@ export default {
         memberInfos[i].order_number = i + 1;
       }
 
-      return memberInfos
+      return memberInfos;
     },
     // 毎月の指定週の指定曜日を計算する関数
     _GetNextMonthDay(currentDate, newWeek, newDay) {
-      const date = parse(currentDate, 'yyyy-MM-dd', new Date());
+      const date = parse(currentDate, "yyyy-MM-dd", new Date());
       // nextで指定された月の最初の曜日を取得(0から6の整数で、日曜日から土曜日までの順)
       const nextMonth = addMonths(date, 1);
       const firstDayOfNextMonth = startOfMonth(nextMonth).getDay();
-      
-      if(newDay < firstDayOfNextMonth && newWeek == 1){
-        newWeek = 2
+
+      if (newDay < firstDayOfNextMonth && newWeek == 1) {
+        newWeek = 2;
       }
-      return format(nextMonth.setDate(newWeek* 7 + (newDay - firstDayOfNextMonth - 7) + 1), 'yyyy-MM-dd')
+      return format(
+        nextMonth.setDate(newWeek * 7 + (newDay - firstDayOfNextMonth - 7) + 1),
+        "yyyy-MM-dd"
+      );
     },
-    _GetNextMonthSpecifiedDate(next, newDate){
-      const date = parse(next, 'yyyy-MM-dd', new Date())
-      date.setDate(newDate)
-      return format(addMonths(date, 1), 'yyyy-MM-dd')
+    _GetNextMonthSpecifiedDate(next, newDate) {
+      const date = parse(next, "yyyy-MM-dd", new Date());
+      date.setDate(newDate);
+      return format(addMonths(date, 1), "yyyy-MM-dd");
     },
     // 指定日からのオフセット日付を計算する関数
-    _GetNextDateByIncrementWeek(currentDate, increment){
-      const date = parse(currentDate, 'yyyy-MM-dd', new Date());
-      return format(addDays(date, increment), 'yyyy-MM-dd');
+    _GetNextDateByIncrementWeek(currentDate, increment) {
+      const date = parse(currentDate, "yyyy-MM-dd", new Date());
+      return format(addDays(date, increment), "yyyy-MM-dd");
     },
-    _GetNextWorkingDate(next){
-      const date = parse(next, 'yyyy-MM-dd', new Date())
+    _GetNextWorkingDate(next) {
+      const date = parse(next, "yyyy-MM-dd", new Date());
       // 金曜日かどうか判定
-      if(date.getDay() == 5){
-        return format(addDays(date, 3), 'yyyy-MM-dd')
-      }else{
-        return format(addDays(date, 1), 'yyyy-MM-dd')
+      if (date.getDay() == 5) {
+        return format(addDays(date, 3), "yyyy-MM-dd");
+      } else {
+        return format(addDays(date, 1), "yyyy-MM-dd");
       }
     },
-    _GetNextDatePerInterval(next, newInterval, newWeek, newDay, newDate){
+    _GetNextDatePerInterval(next, newInterval, newWeek, newDay, newDate) {
       // 平日毎日
-      if(newInterval == 0){
+      if (newInterval == 0) {
         return this._GetNextWorkingDate(next);
-      }else if(newInterval >= 1 && newInterval <= 3){ //毎週、隔週、3週毎
-        return this._GetNextDateByIncrementWeek(next, newInterval*7)
-      }else if(newInterval == 4){ //毎月
+      } else if (newInterval >= 1 && newInterval <= 3) {
+        //毎週、隔週、3週毎
+        return this._GetNextDateByIncrementWeek(next, newInterval * 7);
+      } else if (newInterval == 4) {
+        //毎月
         // 曜日指定
-        if(newWeek != 0){
+        if (newWeek != 0) {
           return this._GetNextMonthDay(next, newWeek, newDay);
-        }else{  //日付指定
+        } else {
+          //日付指定
           return this._GetNextMonthSpecifiedDate(next, newDate);
         }
       }
     },
-    ReSchedule(memberInfos, newInterval, newWeek, newDay, newDate, nextDate){
-      const today = format(startOfToday(), 'yyyy-MM-dd')
+    ReSchedule(memberInfos, newInterval, newWeek, newDay, newDate, nextDate) {
+      const today = format(startOfToday(), "yyyy-MM-dd");
 
       // 今日の日付に一番近い最終実施日のインデックスを検索する（順番入れ替えを考慮するとlastは必ずしも昇順ではない。）
       // reduceは、コールバック関数と初期値を引数に持つ。初期値はコールバック関数の第1引数
       // コールバック関数が最後に return した値が、 reduce() 全体の戻り値になる。
-      const closestLastDateIndex = memberInfos.reduce((closestIndex, memberInfo, index) => {
-        const parsedCurrentLastDate = parse(memberInfo.last, 'yyyy-MM-dd', new Date());
-        const parsedToday  = parse(today, 'yyyy-MM-dd', new Date());
-        // 0番目のlastが今日よりも前ならclosestIndexとしてリターン。そうでないならインデックスを進める。このとき、次回もclosestIndex=indexとなるのでこのパスを通る。
-        if(closestIndex == index){
-          return parsedToday > parsedCurrentLastDate ? index : index+1 
-        }
-        // memberInfos[index].last と parsedClosestLastDateを比較。parsedClosestLastDateの方がtodayに近い場合はclosestIndexを更新
-        const parsedClosestLastDate = parse(memberInfos[closestIndex].last, 'yyyy-MM-dd', new Date());          
-        if (parsedToday > parsedCurrentLastDate && parsedCurrentLastDate > parsedClosestLastDate) {
-          return index;
-        }else{
-          return closestIndex;
-        }
-      }, 0)
-      
-      var next = nextDate
+      const closestLastDateIndex = memberInfos.reduce(
+        (closestIndex, memberInfo, index) => {
+          const parsedCurrentLastDate = parse(
+            memberInfo.last,
+            "yyyy-MM-dd",
+            new Date()
+          );
+          const parsedToday = parse(today, "yyyy-MM-dd", new Date());
+          // 0番目のlastが今日よりも前ならclosestIndexとしてリターン。そうでないならインデックスを進める。このとき、次回もclosestIndex=indexとなるのでこのパスを通る。
+          if (closestIndex == index) {
+            return parsedToday > parsedCurrentLastDate ? index : index + 1;
+          }
+          // memberInfos[index].last と parsedClosestLastDateを比較。parsedClosestLastDateの方がtodayに近い場合はclosestIndexを更新
+          const parsedClosestLastDate = parse(
+            memberInfos[closestIndex].last,
+            "yyyy-MM-dd",
+            new Date()
+          );
+          if (
+            parsedToday > parsedCurrentLastDate &&
+            parsedCurrentLastDate > parsedClosestLastDate
+          ) {
+            return index;
+          } else {
+            return closestIndex;
+          }
+        },
+        0
+      );
+
+      var next = nextDate;
       // closestLastDateIndexがメンバーJSON配列の長さと同値の場合は新規当番作成、もしくは一度も実施していないので全員の次回実施日を設定する
-      if(closestLastDateIndex == memberInfos.length){
-        memberInfos.forEach(memberInfo => {
-          memberInfo.next = next
-          next = this._GetNextDatePerInterval(next, newInterval, newWeek, newDay, newDate)
-        })
-      }else{
+      if (closestLastDateIndex == memberInfos.length) {
+        memberInfos.forEach((memberInfo) => {
+          memberInfo.next = next;
+          next = this._GetNextDatePerInterval(
+            next,
+            newInterval,
+            newWeek,
+            newDay,
+            newDate
+          );
+        });
+      } else {
         // memberInfos[closestLastDateIndex].orderの次のorderから順次リスケ
         // 最初はmemberInfos[closestLastDateIndex].order < memberInfo.orderをリスケ
-        const lastOrder = memberInfos[closestLastDateIndex].order_number
-        var nextOrder = lastOrder + 1
-        
+        const lastOrder = memberInfos[closestLastDateIndex].order_number;
+        var nextOrder = lastOrder + 1;
+
         memberInfos.forEach((memberInfo) => {
-          if(memberInfo.order_number == nextOrder){
-            memberInfo.next = next
-            next = this._GetNextDatePerInterval(next, newInterval, newWeek, newDay, newDate)
-            nextOrder++
+          if (memberInfo.order_number == nextOrder) {
+            memberInfo.next = next;
+            next = this._GetNextDatePerInterval(
+              next,
+              newInterval,
+              newWeek,
+              newDay,
+              newDate
+            );
+            nextOrder++;
           }
-        })
+        });
         // 次にはmemberInfos[closestLastDateIndex].order => memberInfo.orderをリスケ
-        nextOrder = 1
+        nextOrder = 1;
         memberInfos.forEach((memberInfo) => {
-          if(memberInfo.order_number <= lastOrder){
-            memberInfo.next = next
-            next = this._GetNextDatePerInterval(next, newInterval, newWeek, newDay, newDate)
-            nextOrder++
+          if (memberInfo.order_number <= lastOrder) {
+            memberInfo.next = next;
+            next = this._GetNextDatePerInterval(
+              next,
+              newInterval,
+              newWeek,
+              newDay,
+              newDate
+            );
+            nextOrder++;
           }
-        })
+        });
       }
 
-      return memberInfos
+      return memberInfos;
     },
   },
-}
+};
 </script>
--- 00_front/src/views/EditView.vue
+++ 00_front/src/views/EditView.vue
@@ -1,27 +1,31 @@
 <template>
   <div>
-    <calc-member-info ref="RefCaclcMemberInfo"/>
-    <setting ref="RefSetting"/>
+    <calc-member-info ref="RefCaclcMemberInfo" />
+    <setting ref="RefSetting" />
     <v-btn flat rounded="pill" color="primary" @click="onClick">更新</v-btn>
     <v-dialog v-model="dialog" persistent width="600">
-      <input-dialog v-bind:hints="hints" @clickSubmit="onSubmit" @clickClose="onClose"/>
+      <input-dialog
+        v-bind:hints="hints"
+        @clickSubmit="onSubmit"
+        @clickClose="onClose"
+      />
     </v-dialog>
   </div>
 </template>
 
 <script>
-import Setting from '@/components/Setting'
-import CalcMemberInfo from '@/components/module/CalcMemberInfo'
-import InputDialog from '@/components/design/InputDialog'
+import Setting from "@/components/Setting";
+import CalcMemberInfo from "@/components/module/CalcMemberInfo";
+import InputDialog from "@/components/design/InputDialog";
 
 export default {
-  name: 'EditView',
-  components: { 
+  name: "EditView",
+  components: {
     Setting,
     CalcMemberInfo,
     InputDialog,
   },
-  data(){
+  data() {
     return {
       toubanId: -1,
       dialog: false,
@@ -29,148 +33,193 @@ export default {
       current_toubanInfo: {},
       current_memberInfos: [],
       result: {},
-    }
+    };
   },
-  methods:{
-    onClick(){
+  methods: {
+    onClick() {
       // 子(Setting)に入力項目のチェックを要求
-      this.result = this.$refs.RefSetting.Validation()
+      this.result = this.$refs.RefSetting.Validation();
       // status=falseなら終了
-      if(!this.result.status){
+      if (!this.result.status) {
         // alert(`${result.tab}の設定が正しくありません`)
-        return
+        return;
       }
 
       // selectedにオーナーが含まれていない場合はNG
-      if(this.result.tab == "option-2"){
+      if (this.result.tab == "option-2") {
         const foundOwner = this.result.data.some((item) => {
-          return item.name.includes(this.current_toubanInfo.owner)
-        })
-        if(!foundOwner){
-          alert("オーナーが選択されていません")
-          return false
+          return item.name.includes(this.current_toubanInfo.owner);
+        });
+        if (!foundOwner) {
+          alert("オーナーが選択されていません");
+          return false;
         }
       }
-      this.dialog = true
+      this.dialog = true;
     },
-    onClose(){
-      this.dialog = false
+    onClose() {
+      this.dialog = false;
     },
-    onSubmit(){
+    onSubmit() {
       // データベース更新(アクティブタブ単位でデータを取得)
-      switch(this.result.tab){
+      switch (this.result.tab) {
         // 当番名
         case "option-1":
-          var updateToubanInfo = this.current_toubanInfo
-          updateToubanInfo.title = this.result.data
-          this.axios.put("/touban", updateToubanInfo)
-          .then(response => {
-            console.log(response)
-          }).catch(error => console.log(error))
-          break
+          var updateToubanInfo = this.current_toubanInfo;
+          updateToubanInfo.title = this.result.data;
+          this.axios
+            .put("/touban", updateToubanInfo)
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => console.log(error));
+          break;
         // メンバー編集
         case "option-2":
-          const selected = this.result.data
+          const selected = this.result.data;
           // メンバーの更新
-          var updateMemberInfos = this.$refs.RefCaclcMemberInfo.ReplaceMember(this.current_memberInfos, selected)
+          var updateMemberInfos = this.$refs.RefCaclcMemberInfo.ReplaceMember(
+            this.current_memberInfos,
+            selected
+          );
           // 変更したメンバーの次回実施予定日設定
-          const scheduling = this.current_toubanInfo.scheduling
-          var data = scheduling.split("-")
-          var newInterval = parseInt(data[0])
-          var newWeek = parseInt(data[1])
-          var newDay = parseInt(data[2])
-          var newDate = parseInt(data[3])
-          var nextDate = this.current_toubanInfo.next
-          updateMemberInfos = this.$refs.RefCaclcMemberInfo.ReSchedule(updateMemberInfos, newInterval, newWeek, newDay, newDate, nextDate)
+          const scheduling = this.current_toubanInfo.scheduling;
+          var data = scheduling.split("-");
+          var newInterval = parseInt(data[0]);
+          var newWeek = parseInt(data[1]);
+          var newDay = parseInt(data[2]);
+          var newDate = parseInt(data[3]);
+          var nextDate = this.current_toubanInfo.next;
+          updateMemberInfos = this.$refs.RefCaclcMemberInfo.ReSchedule(
+            updateMemberInfos,
+            newInterval,
+            newWeek,
+            newDay,
+            newDate,
+            nextDate
+          );
           // 古いメンバー情報を削除
-          this.axios.delete("/member", {params:{id: this.toubanId}})
-          .then(response => {
-            // 新しいメンバー情報を登録
-            this.axios.post("/member", updateMemberInfos)
-          }).catch(error => console.log(error))
-          break
+          this.axios
+            .delete("/member", { params: { id: this.toubanId } })
+            .then((response) => {
+              // 新しいメンバー情報を登録
+              this.axios.post("/member", updateMemberInfos);
+            })
+            .catch((error) => console.log(error));
+          break;
         // スケジュール設定
         case "option-3":
           // 当番情報の更新
-          var updateToubanInfo = this.current_toubanInfo
-          const newScheduling = this.result.data.scheduling
-          const newStartDate = this.result.data.startDate
-          updateToubanInfo.scheduling = newScheduling
-          updateToubanInfo.next = newStartDate
+          var updateToubanInfo = this.current_toubanInfo;
+          const newScheduling = this.result.data.scheduling;
+          const newStartDate = this.result.data.startDate;
+          updateToubanInfo.scheduling = newScheduling;
+          updateToubanInfo.next = newStartDate;
           // 次回実施日のリスケ
-          var data = newScheduling.split("-")   //shedulingは間隔(sche)、週(week)、曜日(day)、日付(date)の順番
-          var newInterval = parseInt(data[0])
-          var newWeek = parseInt(data[1])
-          var newDay = parseInt(data[2])
-          var newDate = parseInt(data[3])
-          var updateMemberInfos = this.$refs.RefCaclcMemberInfo.ReSchedule(this.current_memberInfos.sort((a, b) => a.order_number - b.order_number), newInterval, newWeek, newDay, newDate, newStartDate)
-          this.axios.put("/touban", updateToubanInfo)
-          .then(response => {
-            this.axios.put("/member", updateMemberInfos)
-            .then(response => {
-              console.log(response)
-            }).catch(error => console.log(error))
-          }).catch(error => console.log(error))
-          break
+          var data = newScheduling.split("-"); //shedulingは間隔(sche)、週(week)、曜日(day)、日付(date)の順番
+          var newInterval = parseInt(data[0]);
+          var newWeek = parseInt(data[1]);
+          var newDay = parseInt(data[2]);
+          var newDate = parseInt(data[3]);
+          var updateMemberInfos = this.$refs.RefCaclcMemberInfo.ReSchedule(
+            this.current_memberInfos.sort(
+              (a, b) => a.order_number - b.order_number
+            ),
+            newInterval,
+            newWeek,
+            newDay,
+            newDate,
+            newStartDate
+          );
+          this.axios
+            .put("/touban", updateToubanInfo)
+            .then((response) => {
+              this.axios
+                .put("/member", updateMemberInfos)
+                .then((response) => {
+                  console.log(response);
+                })
+                .catch((error) => console.log(error));
+            })
+            .catch((error) => console.log(error));
+          break;
         // メッセージ設定
         case "option-4":
-          var updateToubanInfo = this.current_toubanInfo
-          updateToubanInfo.message = this.result.data
-          this.axios.put("/touban", updateToubanInfo)
-          .then(response => {
-            console.log(response)
-          }).catch(error => console.log(error))
-          break
+          var updateToubanInfo = this.current_toubanInfo;
+          updateToubanInfo.message = this.result.data;
+          this.axios
+            .put("/touban", updateToubanInfo)
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => console.log(error));
+          break;
         // 順番変更
         case "option-5":
-          var newOrder = this.result.data
+          var newOrder = this.result.data;
           newOrder.forEach((element, index) => {
-            element.order_number = index + 1
+            element.order_number = index + 1;
           });
           // 次回実施日のリスケ
-          var sch = this.current_toubanInfo.scheduling.split("-")
-          var newInterval = parseInt(sch[0])
-          var newWeek = parseInt(sch[1])
-          var newDay = parseInt(sch[2])
-          var newDate = parseInt(sch[3])
-          var nextDate = this.current_toubanInfo.next
-          var updateMemberInfos = this.$refs.RefCaclcMemberInfo.ReSchedule(newOrder, newInterval, newWeek, newDay, newDate, nextDate)
-          this.axios.put("/member", updateMemberInfos)
-          .then(response => {
-            console.log(response)
-          }).catch(error => console.log(error))
-          break
+          var sch = this.current_toubanInfo.scheduling.split("-");
+          var newInterval = parseInt(sch[0]);
+          var newWeek = parseInt(sch[1]);
+          var newDay = parseInt(sch[2]);
+          var newDate = parseInt(sch[3]);
+          var nextDate = this.current_toubanInfo.next;
+          var updateMemberInfos = this.$refs.RefCaclcMemberInfo.ReSchedule(
+            newOrder,
+            newInterval,
+            newWeek,
+            newDay,
+            newDate,
+            nextDate
+          );
+          this.axios
+            .put("/member", updateMemberInfos)
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => console.log(error));
+          break;
         // オーナー変更
         case "option-6":
-          var updateToubanInfo = this.current_toubanInfo
-          updateToubanInfo.owner = this.result.data.owner
-          updateToubanInfo.password = this.result.data.password
-          updateToubanInfo.cc = updateToubanInfo.cc + ";" + this.result.data.email
-          this.axios.put("/touban", updateToubanInfo)
-          .then(response => {
-            console.log(response)
-          }).catch(error => console.log(error))
-          break
+          var updateToubanInfo = this.current_toubanInfo;
+          updateToubanInfo.owner = this.result.data.owner;
+          updateToubanInfo.password = this.result.data.password;
+          updateToubanInfo.cc =
+            updateToubanInfo.cc + ";" + this.result.data.email;
+          this.axios
+            .put("/touban", updateToubanInfo)
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => console.log(error));
+          break;
         // メール配信設定
         case "option-7":
-          var updateToubanInfo = this.current_toubanInfo
-          updateToubanInfo.mailing = this.result.data.mailing
-          updateToubanInfo.timing = this.result.data.timing
-          updateToubanInfo.cc = this.result.data.cc
-          this.axios.put("/touban", updateToubanInfo)
-          .then(response => {
-            console.log(response)
-          }).catch(error => console.log(error))
-          break
+          var updateToubanInfo = this.current_toubanInfo;
+          updateToubanInfo.mailing = this.result.data.mailing;
+          updateToubanInfo.timing = this.result.data.timing;
+          updateToubanInfo.cc = this.result.data.cc;
+          this.axios
+            .put("/touban", updateToubanInfo)
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => console.log(error));
+          break;
       }
-      this.dialog = false
+      this.dialog = false;
     },
   },
-  created(){
-    this.toubanId = this.$route.params.id
-    const toubanInfo = this.$store.getters.GetToubanByID(this.toubanId)
-    this.current_toubanInfo = toubanInfo[0]  //GetToubanByIDがレコードフィルタ結果を配列で返してくる
-    this.current_memberInfos = this.$store.getters.GetMemberByToubanId(this.toubanId)
-  }
-}
+  created() {
+    this.toubanId = this.$route.params.id;
+    const toubanInfo = this.$store.getters.GetToubanByID(this.toubanId);
+    this.current_toubanInfo = toubanInfo[0]; //GetToubanByIDがレコードフィルタ結果を配列で返してくる
+    this.current_memberInfos = this.$store.getters.GetMemberByToubanId(
+      this.toubanId
+    );
+  },
+};
 </script>
